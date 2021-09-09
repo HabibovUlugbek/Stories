69a0f7e788cc8eb8e33ef99eddd8edeeb72952f8
HabibovUlugbek <72121629+HabibovUlugbek@users.noreply.github.com>
2021-09-09 18:17:40 +0500

    added front and cdn link routes

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
+const path = require('path')
 const express = require("express")
 const dotenv = require("dotenv")
+const morgan = require("morgan")
+const exphbs = require('express-handlebars')
+const connectDB = require("./config/db")
  
 
 // Loading configuration
 dotenv.config({ path:'./config/config.env'})
 
+connectDB()
+
 const app = express();
+
+
+//Logging
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+//Handlebars
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+//Static files
+app.use(express.static(path.join(__dirname,'public')))
+
+//Routes
+app.use('/', require('./routes/index'))
 
 const PORT = process.env.PORT || 5000
 
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const router = express.Router()
+
+//@desc   Login /Landing page
+//route GET /
+router.get('/',(req,res) => {
+    res.render('login')
+})
+
+//@desc   Dashboard
+//route GET /dashboard
+router.get('/dashboard',(req,res) => {
+    res.render('dashboard')
+})
+
+module.exports = router
