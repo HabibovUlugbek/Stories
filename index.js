9095e9b32b343800767afa603a789679981a0823
HabibovUlugbek <72121629+HabibovUlugbek@users.noreply.github.com>
2021-09-10 23:09:07 +0500

    added sidenav and login and logout functionality

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ app.use(express.static(path.join(__dirname,'public')))
 
 //Routes
 app.use('/', require('./routes/index'))
+app.use('/auth',require('./routes/auth'))
 
 const PORT = process.env.PORT || 5000
 
