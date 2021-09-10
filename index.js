cd691ccb8660fe63e6a91779b655c20ce65c01a3
HabibovUlugbek <72121629+HabibovUlugbek@users.noreply.github.com>
2021-09-10 21:56:55 +0500

    added google auth

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const path = require('path')
 const express = require("express")
 const dotenv = require("dotenv")
 const morgan = require("morgan")
+const passport = require('passport')
+const session = require("express-session")
 const exphbs = require('express-handlebars')
 const connectDB = require("./config/db")
  
 
 // Loading configuration
 dotenv.config({ path:'./config/config.env'})
+
+//Passport config
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -22,6 +27,17 @@ if(process.env.NODE_ENV === 'development'){
 //Handlebars
 app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
+
+//Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+  }))
+
+//Passport Middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
 //Static files
 app.use(express.static(path.join(__dirname,'public')))
