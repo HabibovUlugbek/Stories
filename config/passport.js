c1d4cb209d27e8e7cc562502f7bb18a2ed3dff19
HabibovUlugbek <72121629+HabibovUlugbek@users.noreply.github.com>
2021-09-14 15:27:54 +0500

    repair callback url

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ module.exports = function (passport) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL:'/auth/google/callback'
+        callbackURL:'https://lifestorybook.herokuapp.com/auth/google/callback'
     },
     async(accessToken, refreshToken, profile, done) => {
         const newUser = {
